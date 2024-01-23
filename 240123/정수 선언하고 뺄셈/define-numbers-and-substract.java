a686e0adca395d45b5a8a851582f89ed6b34e786
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-01-23 13:32:43 +0100

    Title: 정수 선언하고 뺄셈, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240123/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 13;
+        // int a = 5, b = 9;
+        System.out.print(a + " - "+ b + " = " + (a-b));
+    }
+}
