66573ef9e6f70704183b80a3289b3d0adba44e0a
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-02-14 21:42:09 +0100

    Title: a/b 출력, Time: 140ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240215/a/b 출력/a-divide-b.java
+++ 240215/a/b 출력/a-divide-b.java
@@ -5,7 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         double a = sc.nextDouble(), b = sc.nextDouble();
+        double c = a/b;
 
-        System.out.printf("%.20f", a/b);
+        System.out.printf("%.20f", c);
     }
 }
