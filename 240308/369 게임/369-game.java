f6ea5470f8c6e6732d086633b9c01259bcabe2c8
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-07 23:36:48 +0100

    Title: 369 게임, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 240308/369 게임/369-game.java
+++ 240308/369 게임/369-game.java
@@ -10,7 +10,11 @@ public class Main {
             if (i>10 && i<100){
                 if (i%10 == 3 || i%10 == 6 || i%10 == 9 || i%3==0){
                     System.out.print(0+" ");
-                } else if (i/30==1 || i/60==1 || i/90==1){
+                } else if (i/30==1 && i <40){
+                    System.out.print(0+" ");
+                } else if(i/60==1 && i<70){
+                    System.out.print(0+" ");
+                }else if(i/90==1){
                     System.out.print(0+" ");
                 } else{
                     System.out.print(i+" ");
