35cad140255bd670e7814cf4883fea77f4348bdb
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-10 23:57:20 +0100

    Title: 1까지 나누기, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240311/1까지 나누기/divide-by-1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int tmp = n;
+        int cnt = 0;
+
+        for (int i=1; i<=n; i++){
+            tmp /= i;
+            cnt += 1;
+            if (tmp <= 1){
+                System.out.print(cnt);
+                break;
+            }
+        }
+
+    }
+}
