72746cf3d7fca7836272509e31bcf95eb0e3c891
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-12 22:56:43 +0100

    Title: 2의 거듭제곱수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240313/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = 0;
+
+        while (n != 1){
+            n /= 2;
+            x += 1;
+        }
+
+        System.out.print(x);
+    }
+}
