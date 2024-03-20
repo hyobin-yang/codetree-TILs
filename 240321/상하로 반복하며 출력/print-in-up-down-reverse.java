5b31379716782748be9366adcc443e02ea13651a
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-20 20:29:58 +0100

    Title: 상하로 반복하며 출력, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/상하로 반복하며 출력/print-in-up-down-reverse.java
+++ 240321/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -6,9 +6,10 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i=1; i<=n; i++){
-            for (int j=1; j<=2; j++){
-                System.out.print(i);
-                System.out.print(n+1-i);
+            int cnt = i;
+            for (int j=1; j<=n; j++){
+                System.out.print(cnt);
+                cnt = n+1-cnt;
             }
             System.out.println();
         }
