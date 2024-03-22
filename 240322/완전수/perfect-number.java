994035545b549aee9d82b798eecf11f54281c94b
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-22 14:35:23 +0100

    Title: 완전수, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240322/완전수/perfect-number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt(), end = sc.nextInt();
+        int result = 0;
+
+        for (int i=start; i<=end; i++){
+            int tmp = 0;
+            for (int j=1; j<i; j++){
+                if (i%j == 0){
+                    tmp += j;
+                }
+            }
+            if (tmp == i){
+                result ++;
+            }
+        }
+        System.out.print(result);
+    }
+}
