237502d3604cefbe82e2ff7359bbd790847f4fd2
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-22 13:30:05 +0100

    Title: 구구단으로 이루어진 직각 삼각형, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240322/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = n;
+
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=cnt; j++){
+                if (j==cnt){
+                    System.out.print(i + " * " + j + " = " + i*j);
+                    System.out.println();
+                } else {
+                    System.out.print(i + " * " + j + " = " + i*j + " / ");
+                }
+            }
+            cnt --;
+        }
+    }
+}
