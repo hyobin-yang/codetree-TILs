0e3176fc5ee2b05bbb45ee8296fbd607d40e583a
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-22 14:29:57 +0100

    Title: 3n + 1 수열 m번, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240322/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
+++ 240322/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -4,17 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        int result = 0;
+
 
         for (int i=1; i<=m; i++){
             int n = sc.nextInt();
+            int result = 0;
             while(n!=1){
-            if (n%2 == 0){
-                n /=2;
-            } else{
-                n = n*3 + 1;
-            }
-            result ++;
+                if (n%2 == 0){
+                    n /=2;
+                } else{
+                    n = n*3 + 1;
+                }
+                result ++;
             }
             System.out.println(result);
         }
