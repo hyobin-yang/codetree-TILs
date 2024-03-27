0aa74c08a824eace3f68987a8233af5bb8f4d3a5
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-03-27 18:18:11 +0100

    Title: 10개 입력받기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240328/10개 입력받기/receive-10-inputs.java
+++ 240328/10개 입력받기/receive-10-inputs.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
-        int sum = 0, cnt = 9;
+        int sum = 0, cnt = 10;
 
         for (int i=0; i<10; i++){
             arr[i] = sc.nextInt();
