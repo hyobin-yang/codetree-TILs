dd07c35e99b0a8fb6c6c35e4d3eaa111a908469d
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-04-03 21:19:10 +0200

    Title: 1-9 개수 세기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240404/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[9];
+
+        for (int i=0; i<n; i++){
+            int num = sc.nextInt();
+            arr[num-1] += 1;
+        }
+
+        for (int i=0; i<9; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
