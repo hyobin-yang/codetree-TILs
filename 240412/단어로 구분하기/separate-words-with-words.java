e2ffeb548173e0b8f52ae697c3a1a611e5c82c86
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-04-11 17:05:51 +0200

    Title: 단어로 구분하기, Time: 219ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240412/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s;
+
+        for (int i=0; i<10; i++){
+            s = sc.next();
+            System.out.println(s);
+        }
+    }
+}
