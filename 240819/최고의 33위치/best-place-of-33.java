edd2b01bb15a52f77fa04decd81285724b13a473
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-08-19 11:11:32 +0900

    Title: 최고의 33위치, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

--- 240819/최고의 33위치/best-place-of-33.java
+++ 240819/최고의 33위치/best-place-of-33.java
@@ -1,35 +1,50 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 20;
+
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+
+    // (rowS, colS) ~ (rowE, colE) 사이의 금의 개수를 계산합니다.
+    public static int getNumOfGold(int rowS, int colS, int rowE, int colE) {
+        int numOfGold = 0;
+
+        for(int row = rowS; row <= rowE; row++) {
+            for(int col = colS; col <= colE; col++) {
+                numOfGold += grid[row][col];
+            }
+        }
+        
+        return numOfGold;
+    }
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
+        int maxGold = 0;
 
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                arr[i][j] = sc.nextInt();
+        // 입력
+        n = sc.nextInt();
+        for(int row = 0; row < n; row++)
+            for(int col = 0; col < n; col++)
+                grid[row][col] = sc.nextInt();
+        
+        // (row, col)이 3 * 3 격자의 좌측 모서리인 경우를 전부 탐색합니다. 
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++) {
+                // 3 * 3 격자가 n * n 격자를 벗어나는 경우는 계산하지 않습니다.
+                if(row + 2 >= n || col + 2 >= n)
+                    continue;
+                
+                // 금의 개수를 계산합니다.
+                int numOfGold = getNumOfGold(row, col, row + 2, col + 2);
+
+                // 최대 금의 개수를 저장합니다.
+                maxGold = Math.max(maxGold, numOfGold);
             }
         }
 
-        int max = 0;
-
-        for (int a=0; a<=n-3; a++){
-            int sum = 0;
-            for (int i=a; i<=a+2; i++){
-                for (int b=0; b<=n-3; b++){
-                    for (int j=b; j<=b+2; j++){
-                        sum += arr[i][j];
-                    }
-                }
-            }
-            if (sum > max){
-                    max = sum;
-            }
-        }
-
-        System.out.print(max);
+        System.out.println(maxGold);
     }
 }
