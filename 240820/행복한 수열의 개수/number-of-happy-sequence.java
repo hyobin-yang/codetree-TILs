f390494665d6d3474a8f5b0d9a6fd19b7e722701
hyobinYang <101079399+hyobin-yang@users.noreply.github.com>
2024-08-20 10:41:20 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240820/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240820/행복한 수열의 개수/number-of-happy-sequence.java
@@ -11,7 +11,7 @@ public class Main {
         return cnt == m;
     }
 
-    public static int happySequence(int i){
+    public static void happySequence(int i){
         
         int prev;
         int current;
